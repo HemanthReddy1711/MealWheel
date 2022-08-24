1a161c66937281eb467b37bb8c5ed247d6c16e60
HemanthReddy1711 <reddy.reddy@globallogic.com>
2022-08-24 20:05:45 +0530

    not completed

--- MealWheel/Controllers/UserProfileController.cs
+++ MealWheel/Controllers/UserProfileController.cs
@@ -2,6 +2,7 @@
 using MealWheel.Models;
 using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 using MealWheel.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace MealWheel.Controllers
 {
@@ -11,32 +12,42 @@ namespace MealWheel.Controllers
         IHostingEnvironment _env;
         public ApplicationDbContext _applicationDb;
         public ApplicationUser applicationUser;
-        public UserProfileController(MealDbContext meal, IHostingEnvironment env,ApplicationDbContext applicationDb)
+        public UserManager<ApplicationUser> UserManager;
+        public UserProfileController(MealDbContext meal, IHostingEnvironment env,ApplicationDbContext applicationDb, UserManager<ApplicationUser> _UserManager)
         {
             _meal = meal;
             _env = env;
             _applicationDb=applicationDb;
+            UserManager = _UserManager;
         }
 
         public IActionResult Index()
         {
             string uname = HttpContext.User.Identity.Name.ToString();
-            MyProfile myProfile = _meal.myProfiles.FirstOrDefault(e=>e.email==uname);
-
-            return View(myProfile);
+            ApplicationUser applicationUser = new ApplicationUser();
+            applicationUser.Firstname = _applicationDb.Users.FirstOrDefault(e => e.Email == uname).Firstname;
+            applicationUser.Lastname = _applicationDb.Users.FirstOrDefault(e => e.Email == uname).Lastname;
+            applicationUser.Email = _applicationDb.Users.FirstOrDefault(e => e.Email == uname).Email;
+            applicationUser.Address = _applicationDb.Users.FirstOrDefault(e => e.Email == uname).Address;
+            applicationUser.profileurl = _applicationDb.Users.FirstOrDefault(e => e.Email == uname).profileurl;
+            applicationUser.MobileNumber = _applicationDb.Users.FirstOrDefault(e => e.Email == uname).MobileNumber;
+            return View(applicationUser);
         }
         [HttpPost]
-        public IActionResult Index(MyProfile myProfile)
+        public IActionResult Index(ApplicationUser myProfile)
         {
-            ApplicationUser a = new ApplicationUser();
-            if(myProfile.profileImage!=null)
+            string uname = HttpContext.User.Identity.Name.ToString();
+            string uid = UserManager.GetUserId(HttpContext.User);
+            myProfile.Id = uid;
+            //c47ff13e-a94d-4964-8d8e-be82d530524c
+            if (myProfile.profileImage!=null)
             {
                 var nam = Path.Combine(_env.WebRootPath + "/Images", Path.GetFileName(myProfile.profileImage.FileName));
                 myProfile.profileImage.CopyTo(new FileStream(nam, FileMode.Create));
                 myProfile.profileurl = "Images/" + myProfile.profileImage.FileName;
             }
-            _meal.myProfiles.Update(myProfile);
-            _meal.SaveChanges();
+            _applicationDb.Users.Update(myProfile);
+            _applicationDb.SaveChanges();
             return View(myProfile);
         }
     }
