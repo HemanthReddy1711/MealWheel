732facc752c1ffac9d7bce6caca38e18d6edf213
HemanthReddy1711 <reddy.reddy@globallogic.com>
2022-08-25 18:21:53 +0530

    Microservices started

---
+++ feeedbackapi/Controllers/FeedbackController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using feeedbackapi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace feeedbackapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+            private readonly MealWheelDBContext _context;
+
+            public FeedbackController(MealWheelDBContext context)
+            {
+                _context = context;
+            }
+
+            //GET: api/Products
+            [HttpGet]
+            public async Task<ActionResult<IEnumerable<Feedback>>> GetFeedbacks()
+            {
+                return await _context.Feedbacks.ToListAsync();
+            }
+
+            //GET: api/Products/1
+            [HttpGet("{Id}")]
+            public async Task<ActionResult<Feedback>> GetFeedbacks(int id)
+            {
+                var product = await _context.Feedbacks.FindAsync(id);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                return product;
+            }
+
+        }
+    }
